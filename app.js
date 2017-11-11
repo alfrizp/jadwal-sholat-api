42469b5ca868c52fb8681b8dd664928e3714f557
alip <alifrizkipambudi@gmail.com>
2017-11-11 14:04:17 +0700

    get api sholat

--- app.js
+++ app.js
@@ -16,3 +16,33 @@ const argv = yargs
 
 const encodedAddress = encodeURIComponent(argv.a)
 const geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`
+
+axios.get(geocodeUrl)
+  .then(res => {
+    let lat = res.data.results[0].geometry.location.lat
+    let lng = res.data.results[0].geometry.location.lng
+    let formattedAddress = res.data.results[0].formatted_address
+
+    let timestampUrl = 'http://api.aladhan.com/currentTimestamp?zone=Asia/Jakarta'
+
+    console.log(`Address : ${formattedAddress}`)
+
+    axios.get(timestampUrl)
+      .then(res => {
+        let timestamp = res.data.data
+
+        let prayTimeUrl = `http://api.aladhan.com/timings/${timestamp}?latitude=${lat}&longitude=${lng}&method=1`
+
+        axios.get(prayTimeUrl)
+          .then(res => {
+            let prayTime = res.data.data.timings
+
+            console.log('Sholah Prayer Time : ')
+            console.log(`- Subuh : ${prayTime.Fajr}`)
+            console.log(`- Dzuhur : ${prayTime.Dhuhr}`)
+            console.log(`- 'Asar : ${prayTime.Asr}`)
+            console.log(`- Magrib : ${prayTime.Maghrib}`)
+            console.log(`- Isya' : ${prayTime.Isha}`)
+          })
+      })
+  })
